ac70df4b3c5501b54aeea370221b18b59500b359
Romario Gustave <romagustave10@gmail.com>
2021-03-17 20:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your classes here
+class Tree {
+  constructor(species) {{
+    this._species = species
+  }
+    static definition() {
+    return `${super.definition()} a road vehicle, typically with four wheels, powered by an internal combustion engine and able to carry a small number of people.`}
+  }
